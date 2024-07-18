f11a432fa898722cd580c26a40abd1b56dd22c44
Emerson1911 <1284662@clases.edu.sv>
2024-07-18 11:20:43 -0600

    Cambios Brutales

--- test/script.js
+++ test/script.js
@@ -1,27 +1,9 @@
-const nombreInput = document.getElementById('nombre');
-const apellidoInput = document.getElementById('apellido');
-const rows = document.querySelectorAll('tbody tr');
-
-nombreInput.addEventListener('input', () => {
-    const filtroNombre = nombreInput.value.trim().toLowerCase();
-    rows.forEach(row => {
-        const nombre = row.cells[0].textContent.trim().toLowerCase();
-        if (nombre.includes(filtroNombre)) {
-            row.style.display = '';
-        } else {
-            row.style.display = 'none';
-        }
-    });
-});
-
-apellidoInput.addEventListener('input', () => {
-    const filtroApellido = apellidoInput.value.trim().toLowerCase();
-    rows.forEach(row => {
-        const apellido = row.cells[1].textContent.trim().toLowerCase();
-        if (apellido.includes(filtroApellido)) {
-            row.style.display = '';
-        } else {
-            row.style.display = 'none';
-        }
+document.addEventListener('DOMContentLoaded', function() {
+    const buscarBtn = document.getElementById('buscar');
+    const nombreInput = document.getElementById('nombre');
+    
+    buscarBtn.addEventListener('click', function() {
+        const nombre = nombreInput.value;
+        alert(`¡Soy ! ${nombre}`);
     });
 });
